fe832ab69d1a4797a5d8df7e0efcef36935cb9ad
Marco <marcovillafa13@hotmail.com>
2024-05-08 19:26:45 -0300

    primercommit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { resolve } from 'path';
+
+const nextConfig = {
+  // Configura Next.js para que procese los estilos de Tailwind CSS
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias['@emotion/react'] = resolve('@emotion/react');
+      config.resolve.alias['@emotion/styled'] = resolve('@emotion/styled');
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
       },
       backgroundImage: {
         "gradient-cover":
-          "linear-gradient(90.21deg, rgba(70, 54, 124, 0.5) -5.91%, rgba(74, 47, 189, 0.5) 111.58% )"
+          "linear-gradient(90.21deg, rgba(170, 54, 124, 0.5) -5.91%, rgba(74, 47, 189, 0.5) 111.58%)",
+        // Se corrigió la coma entre los valores de color y posición
       },
     },
   },
